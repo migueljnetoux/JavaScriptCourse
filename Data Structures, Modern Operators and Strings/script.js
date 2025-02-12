a51f2119064353b7e2f28125c286f0e79587a69c
migueljnetoux <migueljneto.ux@gmail.com>
2025-02-12 16:21:03 +0000

    Optional Chaining

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -5,6 +5,22 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, //24 hours open
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,41 +28,23 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, //24 hours open
-      close: 24,
-    },
-  },
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    adress,
-    starterIndex,
-    mainIndex,
-    hourOfDelivery,
-  }) {
+  orderDelivery({ adress, starterIndex, mainIndex, hourOfDelivery }) {
     console.log(
       `adress: ${adress}, starter: ${this.starterMenu[starterIndex]}, main: ${this.mainMenu[mainIndex]}, hours: ${hourOfDelivery}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Pasta will have ${ing1}, ${ing2} and ${ing3}`);
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(
       `The pizza will have ${mainIngredient} and ${otherIngredients}`
     );
@@ -54,7 +52,27 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-/* 
+
+//optional chaning
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open); // .open only runs if ...? exists
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of weekdays) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(open);
+}
+
+console.log(restaurant.order?.(0, 1) ?? 'Does not exist');
+console.log(restaurant.orderFood?.(0, 1) ?? 'Does not exist');
+
+const users = [{ name: 'Jonas', email: 'hello.io' }];
+
+console.log(users[0]?.name ?? 'Not Defined');
+
+/*
 //for of loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
