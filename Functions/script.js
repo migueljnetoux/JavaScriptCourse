f14e7bb6b7ae1e3f908ccd93cb2831668157ddc9
migueljnetoux <migueljneto.ux@gmail.com>
2025-02-19 13:48:55 +0000

    Value vs Reference

--- Functions/script.js
+++ Functions/script.js
@@ -21,3 +21,33 @@
 // create_booking('TP099', 2);
 // create_booking('TP099', undefined);
 // // console.log(bookings);
+
+// Passing Arguments : Value vs Reference
+
+let flight = 'LH123';
+const jonas = {
+  name: 'Jonas',
+  passport: 321789654,
+};
+//passing a primitive type === passing a copy
+//passing a object type === passing the object
+const check_In = function (flight_num, passenger) {
+  flight_num = 'LH199';
+  passenger.name = 'Mr.' + passenger.name;
+
+  //   passenger.passport === 321789654
+  //     ? alert('Checked in')
+  //     : alert('Wrong passport');
+};
+
+// check_In(flight, jonas);
+
+// console.log(flight);
+// console.log(jonas);
+
+const new_passport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+new_passport(jonas);
+check_In(flight, jonas);
