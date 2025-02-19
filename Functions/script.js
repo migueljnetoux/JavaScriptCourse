039cf2869b5860a1de637c76c0193521b64d2afa
migueljnetoux <migueljneto.ux@gmail.com>
2025-02-19 14:48:18 +0000

    Callback Functions

--- Functions/script.js
+++ Functions/script.js
@@ -24,30 +24,60 @@
 
 // Passing Arguments : Value vs Reference
 
-let flight = 'LH123';
-const jonas = {
-  name: 'Jonas',
-  passport: 321789654,
-};
-//passing a primitive type === passing a copy
-//passing a object type === passing the object
-const check_In = function (flight_num, passenger) {
-  flight_num = 'LH199';
-  passenger.name = 'Mr.' + passenger.name;
+// let flight = 'LH123';
+// const jonas = {
+//   name: 'Jonas',
+//   passport: 321789654,
+// };
+// //passing a primitive type === passing a copy
+// //passing a object type === passing the object
+// const check_In = function (flight_num, passenger) {
+//   flight_num = 'LH199';
+//   passenger.name = 'Mr.' + passenger.name;
 
-  //   passenger.passport === 321789654
-  //     ? alert('Checked in')
-  //     : alert('Wrong passport');
-};
+//   //   passenger.passport === 321789654
+//   //     ? alert('Checked in')
+//   //     : alert('Wrong passport');
+// };
 
+// // check_In(flight, jonas);
+
+// // console.log(flight);
+// // console.log(jonas);
+
+// const new_passport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000);
+// };
+
+// new_passport(jonas);
 // check_In(flight, jonas);
 
-// console.log(flight);
-// console.log(jonas);
+//higher order functions
 
-const new_passport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
-};
+// const one_word = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-new_passport(jonas);
-check_In(flight, jonas);
+// const upper_first_word = function (str) {
+//   const [first, ...rest] = str.split(' ');
+//   return [first.toUpperCase(), ...rest].join(' ');
+// };
+
+// // HIGHER ORDER FUNCTION
+
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+
+//   console.log(`-`.repeat(10));
+// };
+
+// transformer('python is really fun', upper_first_word /* callback function */);
+// transformer('python is really fun', one_word /* callback function */);
+
+// const high_five = function () {
+//   console.log('hi');
+// };
+
+// document.body.addEventListener('click', high_five);
