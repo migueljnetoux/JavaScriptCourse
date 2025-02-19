1ef862407bb76a79c53cef279028c56c44bfe189
migueljnetoux <migueljneto.ux@gmail.com>
2025-02-19 13:25:05 +0000

    Default Parameters

---
+++ Functions/script.js
@@ -0,0 +1,23 @@
+'use strict';
+// //default parameters
+// const bookings = [];
+// const create_booking = function (
+//   flight_number,
+//   n_passengers = 1,
+//   price = 500 * n_passengers
+// ) {
+//   const booking = {
+//     flight_number,
+//     n_passengers,
+//     price,
+//   };
+//   console.log(booking);
+
+//   bookings.push(booking);
+// };
+
+// create_booking('LH123');
+// create_booking('TP099', 2, 300);
+// create_booking('TP099', 2);
+// create_booking('TP099', undefined);
+// // console.log(bookings);
